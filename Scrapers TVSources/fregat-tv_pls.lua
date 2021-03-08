63eeadae55ff24309cc35bf164d6d8cbc9c6a806
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-08 11:02:47 +0300

    Update fregat-tv_pls.lua

--- Scrapers TVSources/fregat-tv_pls.lua
+++ Scrapers TVSources/fregat-tv_pls.lua
@@ -1,4 +1,4 @@
--- скрапер TVS для загрузки плейлиста "Фрегат ТВ" https://fregat.com (12/1/21)
+-- скрапер TVS для загрузки плейлиста "Фрегат ТВ" https://fregat.com (8/3/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## переименовать каналы ##
 local filter = {
@@ -25,9 +25,13 @@ local filter = {
 	function GetVersion()
 	 return 2, 'UTF-8'
 	end
-	local function showMess(str, color)
+	local function showMsg(str, color)
 		local t = {text = str, color = color, showTime = 1000 * 5, id = 'channelName'}
 		m_simpleTV.OSD.ShowMessageT(t)
+	end
+	local function extOpt()
+	 return decode64('JE9QVDpodHRwLWV4dC1oZWFkZXI9eC1zbWFydGxhYnMtbWFjLWFkZHJlc3M6RjU6MTE6QTA6MEY6')
+					..  string.format('%02X:%02X', math.random(255), math.random(255))
 	end
 	local function LoadFromSite()
 		local session = m_simpleTV.Http.New('SmartLabs/1.51652.472 (sml723x, SML-482) SmartSDK/1.5.63-rt-25 Qt/4.7.3 API/20121210')
@@ -38,7 +42,6 @@ local filter = {
 		m_simpleTV.Http.Close(session)
 			if rc ~= 200 then return end
 		local t, i = {}, 1
-		local extOpt = decode64('JE9QVDpodHRwLWV4dC1oZWFkZXI9eC1zbWFydGxhYnMtbWFjLWFkZHJlc3M6RjU6MTE6QTA6MEY6MjI6RkY')
 		local logoHost = url:match('^https?://[^/]+')
 			for w in answer:gmatch('<channel>.-</channel>') do
 				local name = w:match('<bcname>([^<]+)')
@@ -46,7 +49,7 @@ local filter = {
 				if adr and name then
 					t[i] = {}
 					t[i].name = name:gsub('amp;', '')
-					t[i].address = adr .. extOpt
+					t[i].address = adr .. extOpt()
 					local logo = w:match('<logo2>([^<]+)') or ''
 					if logo ~= '' then
 						t[i].logo = string.format('%s/images/%s', logoHost, logo)
@@ -64,11 +67,11 @@ local filter = {
 		local Source = TVSources_var.tmp.source[UpdateID]
 		local t_pls = LoadFromSite()
 			if not t_pls then
-				showMess(Source.name .. ' ошибка загрузки плейлиста', ARGB(255, 255, 102, 0))
+				showMsg(Source.name .. ' ошибка загрузки плейлиста', ARGB(255, 255, 102, 0))
 			 return
 			end
 		t_pls = ProcessFilterTableLocal(t_pls)
-		showMess(Source.name .. ' (' .. #t_pls .. ')', ARGB(255, 153, 255, 153))
+		showMsg(Source.name .. ' (' .. #t_pls .. ')', ARGB(255, 153, 255, 153))
 		local m3ustr = tvs_core.ProcessFilterTable(UpdateID, Source, t_pls)
 		local handle = io.open(m3u_file, 'w+')
 			if not handle then return end
