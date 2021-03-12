b1e2233f31aa959de326382b4acd9c7d965bc01d
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-12 12:46:32 +0300

    Add files via upload

--- Scrapers TVSources/tv+hd_pls.lua
+++ Scrapers TVSources/tv+hd_pls.lua
@@ -1,7 +1,8 @@
--- скрапер TVS для загрузки плейлиста "TV+ HD" http://www.tvplusonline.ru (7/3/21)
+-- скрапер TVS для загрузки плейлиста "TV+ HD" http://www.tvplusonline.ru (10/3/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- видоскрипт: tv+hd.lua
+-- расширение дополнения httptimeshift: tvhd-timeshift_ext.lua
 -- ## переименовать каналы ##
 local filter = {
 	{'Майдан', 'Майдан (Казань)'},
@@ -28,7 +29,7 @@ local filter = {
 	 return t
 	end
 	function GetSettings()
-	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\TVplus.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, show_progress = 0, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 1, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
+	 return {name = my_src_name, sortname = '', scraper = '', m3u = 'out_' .. my_src_name .. '.m3u', logo = '..\\Channel\\logo\\Icons\\TVplus.png', TypeSource = 1, TypeCoding = 1, DeleteM3U = 1, RefreshButton = 1, show_progress = 0, AutoBuild = 0, AutoBuildDay = {0, 0, 0, 0, 0, 0, 0}, LastStart = 0, TVS = {add = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, LogoTVG = 1}, STV = {add = 1, ExtFilter = 1, FilterCH = 1, FilterGR = 1, GetGroup = 1, HDGroup = 0, AutoSearch = 1, AutoNumber = 0, NumberM3U = 0, GetSettings = 0, NotDeleteCH = 0, TypeSkip = 1, TypeFind = 1, TypeMedia = 0, RemoveDupCH = 1}}
 	end
 	function GetVersion()
 	 return 2, 'UTF-8'
@@ -38,30 +39,87 @@ local filter = {
 		m_simpleTV.OSD.ShowMessageT(t)
 	end
 	local function LoadFromSite()
-		local session = m_simpleTV.Http.New('Dalvik/2.1.0 (Linux; Android 7.1.2;)')
+		local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0')
 			if not session then return end
 		m_simpleTV.Http.SetTimeout(session, 8000)
 		local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2FwaS9jaGFubmVscw')})
-		m_simpleTV.Http.Close(session)
-			if rc ~= 200 then return end
+			if rc ~= 200 then
+				m_simpleTV.Http.Close(session)
+			 return
+			end
 		answer = answer:gsub('%[%]', '')
 		answer = answer:gsub('\u0', '\\u0')
 		require 'json'
 		local tab = json.decode(answer)
 			if not tab then return end
-		local t, i = {}, 1
+		local dvr
+		rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L3ZlcnNpb250di50eHQ')})
+		m_simpleTV.Http.Close(session)
+			if rc == 200 then
+				dvr = answer:match('dvr,([^%c]+)')
+			end
+		dvr = dvr or 'moscow24;kinopremier;kinohit;kinofamily;kinolove;kinoman;matchtvhd;boxtv;m1global;matchfighter;matchoursport;matchgamehd;matchgame;matcharenahd;matcharena;matchpremier;matchfootball1;matchfootball2;matchfootball3;khlhd;kinomix;nashenovoekino;rodnoekino;kinokomediya;indiankino;kinoseriya;kinouzas'
+		dvr = split(dvr, ';')
+			local function catchup(adr)
+				for i = 1, #dvr do
+					if adr == dvr[i] then
+					 return 'catchup="append" catchup-days="1" catchup-source=""'
+					end
+				end
+			 return
+			end
+		local t = {}
 			for j = 1, #tab do
 				local title = tab[j].title
 				local adr = tab[j].name
-				local closed = tab[j].closed
-				if title and adr and closed == 0 then
-					t[i] = {}
-					t[i].name = unescape1(title)
-					t[i].address = string.format('https://tv+hd.%s', adr)
-					i = i + 1
+				if title and adr then
+					t[#t +1] = {}
+					t[#t].name = unescape1(title)
+					t[#t].address = string.format('https://tv+hd.%s', adr)
+					t[#t].RawM3UString = catchup(adr)
 				end
 			end
 			if #t == 0 then return end
+		local plus = {
+				{'ntvp2', 'НТВ (+2)'},
+				{'ntvp4', 'НТВ (+4)'},
+				{'ntvp6', 'НТВ (+6)'},
+				{'perviyp2', 'Первый (+2)'},
+				{'perviyp4', 'Первый (+4)'},
+				{'perviyp6', 'Первый (+6)'},
+				{'renp2', 'РЕН ТВ (+2)'},
+				{'renp4', 'РЕН ТВ (+4)'},
+				{'renp6', 'РЕН ТВ (+6)'},
+				{'russiap2', 'Россия 1 (+2)'},
+				{'russiap4', 'Россия 1 (+4)'},
+				{'russiap6', 'Россия 1 (+6)'},
+				{'stsp2', 'СТС (+2)'},
+				{'stsp4', 'СТС (+4)'},
+				{'stsp6', 'СТС (+6)'},
+				{'tntp2', 'ТНТ (+2)'},
+				{'tntp4', 'ТНТ (+4)'},
+				{'tntp6', 'ТНТ (+6)'},
+				{'tv3p2', 'ТВ-3 (+2)'},
+				{'tv3p4', 'ТВ-3 (+4)'},
+				{'tv3p6', 'ТВ-3 (+6)'},
+				{'karuselp2', 'Карусель (+2)'},
+				{'karuselp4', 'Карусель (+4)'},
+				{'karuselp6', 'Карусель (+6)'},
+				{'centrtv', 'Центральное телевидение'},
+				{'fridayp2', 'Пятница! (+2)'},
+				{'fridayp4', 'Пятница! (+4)'},
+				{'fridayp6', 'Пятница! (+6)'},
+				{'stshd', 'СТС HD'},
+				{'renhd', 'РЕН ТВ HD'},
+				{'russia', 'Россия 1'},
+				{'tv3hd', 'ТВ-3 HD'},
+				{'bridgetvhd', 'Bridge TV Deluxe'},
+				{'perviy', 'Первый'},
+				{'fridayhd', 'Пятница! HD'},
+			}
+			for j = 1, #plus do
+				table.insert(t, {name = plus[j][2], address = string.format('https://tv+hd.%s', plus[j][1])})
+			end
 	 return t
 	end
 	function GetList(UpdateID, m3u_file)
