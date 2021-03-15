e9a9a5c6e6adf67ead1b8c4a0a08848a05a5b522
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-15 16:32:21 +0300

    Update tv+hd.lua

--- Video Scripts/tv+hd.lua
+++ Video Scripts/tv+hd.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (10/3/21)
+-- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (15/3/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: tv+hd_pls.lua
@@ -12,26 +12,17 @@
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, TypeBackColor = 0, PictFileName = '', UseLogo = 0, Once = 1})
 	end
-	if not m_simpleTV.User then
-		m_simpleTV.User = {}
-	end
-	if not m_simpleTV.User.tvhd then
-		m_simpleTV.User.tvhd = {}
-	end
-	m_simpleTV.User.tvhd.address = nil
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
 	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0'
 	local session = m_simpleTV.Http.New(userAgent)
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local id = inAdr:gsub('.+%.(%w+)', '%1')
+	local id = inAdr:match('%.(%w+)')
 	local url = decode64('aHR0cHM6Ly93d3cudHZwbHVzc3RyZWFtaW5nLnJ1L2dldHNpZ25lZHVybGNkbnY0LnBocD9xPTAmcD1hJnM9MCZjPQ') .. id
-	url = url:gsub('$OPT:.+', '')
 	local rc, retAdr = m_simpleTV.Http.Request(session, {url = url})
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
-	m_simpleTV.User.tvhd.address = id
-	retAdr = string.format('%s$OPT:no-gnutls-system-trust$OPT:http-user-agent=%s', retAdr:gsub('%c', ''), userAgent)
+	retAdr = retAdr:gsub('%c', '')
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
