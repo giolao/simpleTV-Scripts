677eb1287616970dd5cf6ac551bd5d1adf927b2e
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-21 22:43:41 +0300

    Update strahtv.lua

--- Video Scripts/strahtv.lua
+++ Video Scripts/strahtv.lua
@@ -1,20 +1,21 @@
--- видеоскрипт для плейлиста "Страх ТВ" https://strah.video (15/2/21)
+-- видеоскрипт для плейлиста "Страх ТВ" https://strah.video (21/3/21)
 -- Copyright © 2017-2021 Nexter | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: strah_pls.lua
 -- модуль: /core/playerjs.lua
--- открывает подобные ссылки:
+-- ## открывает подобные ссылки ##
 -- https://strah.video/id/1
+-- ##
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://strah%.video/id/%d') then return end
 	if m_simpleTV.Control.MainMode == 0 then
-		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'https://strah.video/style/strah.png', TypeBackColor = 0, UseLogo = 1, Once = 1})
+		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'https://raw.githubusercontent.com/Nexterr-origin/simpleTV-Images/main/srtahtv.png', TypeBackColor = 0, UseLogo = 1, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	require 'playerjs'
 	m_simpleTV.Control.ChangeAdress = 'Yes'
 	m_simpleTV.Control.CurrentAdress = 'error'
-	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:85.0) Gecko/20100101 Firefox/85.0'
+	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0'
 	local session = m_simpleTV.Http.New(userAgent)
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 12000)
