6b60bc6084773d8aab5675a7819f88673bb77353
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-24 20:41:48 +0300

    Update tv+hd.lua

--- Video Scripts/tv+hd.lua
+++ Video Scripts/tv+hd.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (20/3/21)
+-- видеоскрипт для плейлиста "TV+ HD" http://www.tvplusonline.ru (24/3/21)
 -- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: tv+hd_pls.lua
@@ -14,15 +14,13 @@
 	end
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0'
-	local session = m_simpleTV.Http.New(userAgent)
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
 	local id = inAdr:match('%.(%w+)')
 	local url
 	if inAdr:match('&plus=true') then
-		url = string.format(decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2dldHNpZ25lZGR2ci5waHA/ZHVyYXRpb249NjAwJnN0cmVhbT0lcyZ0aW1lPSVz'), id, os.time() - 600)
-		-- url = decode64('aHR0cHM6Ly93d3cudHZwbHVzc3RyZWFtaW5nLnJ1L2dldHNpZ25lZHVybGNkbnY0LnBocD9xPTAmcD1hJnM9MCZjPQ') .. id
+		url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2dldHNpZ25lZGR2ci5waHA/c3RyZWFtPQ') .. id
 	else
 		url = decode64('aHR0cHM6Ly93d3cudHZwbHVzb25saW5lLnJ1L2FwaS9jaGFubmVscy9obHMv') .. id
 	end
@@ -32,7 +30,6 @@
 	answer = answer:gsub('\\/', '/')
 	local retAdr = answer:match('https?://[^%s"]+')
 		if not retAdr then return end
-	retAdr = retAdr:gsub('^(.+)%-dvr/.-(%..-)$', '%1-dvr/timeshift_rel-20%2')
-	retAdr = retAdr:gsub('/index.-%.m3u8', '/index.m3u8')
+	retAdr = retAdr:gsub('/index[^.]+%.', '/index.')
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
